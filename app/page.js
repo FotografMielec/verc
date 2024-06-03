fdada566dc999e236b2e8ac1625dc4b071bb0dfb
FotografMielec <jankokowalski1986@op.pl>
2024-06-03 14:28:10 +0200

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,182 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<>
+			<header>
+				<div className="header-image">
+					<h1>10 Pytań, Które Musisz Zadać Przed Wyborem Fotografa Ślubnego</h1>
+				</div>
+			</header>
+
+			<main className="container">
+				<section>
+					<h2>Wprowadzenie</h2>
+					<p>
+						Planując swój wielki dzień, wybór fotografa ślubnego może być jedną
+						z najważniejszych decyzji, jakie podejmiesz. To on będzie
+						odpowiedzialny za uchwycenie niezapomnianych momentów, które chcesz
+						zachować na całe życie. Aby upewnić się, że wybierasz odpowiedniego
+						fotografa, postaraj się zadać mu te 10 kluczowych pytań.
+					</p>
+				</section>
+
+				<section>
+					<h2>Doświadczenie i Portfolio</h2>
+					<p>
+						<span className="keyword">
+							Czy możesz pokazać mi swoje portfolio z poprzednich ślubów?{" "}
+						</span>
+						<br />
+						Nie ma nic lepszego niż rzeczywiste przykłady pracy fotografa.
+						Poproś o pokazanie portfolio z różnych ślubów, abyś mógł zobaczyć,
+						jakie historie potrafi opowiedzieć za pomocą obrazów. Spójrz na
+						różnorodność zdjęć - od formalnych portretów po spontaniczne ujęcia.
+						To pomoże Ci zrozumieć, czy styl fotografa pasuje do Twojej wizji.
+					</p>
+					<p>
+						<span className="keyword">
+							Jaki jest Twój preferowany styl fotografii ślubnej?{" "}
+						</span>
+						<br />
+						Fotografowie mają różne podejścia do dokumentowania ślubów.
+						Niektórzy preferują tradycyjny, formalny styl, podczas gdy inni
+						skupiają się na uchwyceniu naturalnych, emocjonalnych momentów.
+						Zastanów się, czy wolisz klasyczne ujęcia czy też bardziej
+						spontaniczne. Jeśli masz jakieś konkretne preferencje dotyczące
+						stylu fotografii, koniecznie powiedz o tym fotografowi.
+					</p>
+					<p>
+						<span className="keyword">
+							Czy jesteś dostępny w dniu naszego ślubu?
+						</span>
+						<br />
+						Nie zapomnij zapytać o dostępność na konkretną datę. Dobre fotografy
+						często rezerwują terminy z dużym wyprzedzeniem, więc im wcześniej
+						się z nimi skontaktujesz, tym większa szansa, że uda Ci się
+						zarezerwować termin. Zapytaj również, czy fotograf będzie dostępny
+						przez całą ceremonię i przyjęcie.
+					</p>
+				</section>
+
+				<section>
+					<p>
+						<span className="keyword">
+							Jak długo będziesz fotografować podczas naszego ślubu?{" "}
+						</span>
+						<br />
+						Upewnij się, że wiesz, ile czasu fotograf będzie spędzał na Twoim
+						ślubie. Czy będzie obecny tylko podczas ceremonii i przyjęcia, czy
+						też planuje sfotografowanie przygotowań i weselnego tańca? Dobry
+						fotograf będzie elastyczny i dostosuje się do Twoich potrzeb.
+					</p>
+					<p>
+						<span className="keyword">
+							Czy oferujesz dodatkowe usługi, takie jak sesja narzeczeńska czy
+							albumy ślubne?
+						</span>
+						<br />
+						Niektórzy fotografowie oferują dodatkowe usługi, które mogą uczynić
+						Twoje wspomnienia jeszcze bardziej wyjątkowymi. Sesje narzeczeńskie
+						mogą być doskonałą okazją do zapoznania się z fotografem i
+						zwiększenia komfortu przed obiektywem. Albumy ślubne to piękne
+						pamiątki, które będą cieszyć Was przez lata.
+					</p>
+					<p>
+						<span className="keyword">
+							Jakie są koszty usługi fotograficznej, i co jest wliczone w cenę?{" "}
+						</span>
+						<br />
+						Zrozumienie kosztów usługi fotograficznej to kluczowy krok w
+						planowaniu budżetu na ślub. Poproś o jasny zarys cenowy oraz o
+						informacje na temat tego, co jest wliczone w cenę usługi. Upewnij
+						się również, że wszystkie ustalenia są spisane w umowie.
+					</p>
+				</section>
+
+				<section>
+					<p>
+						<span className="keyword">
+							Co zrobisz w przypadku awarii sprzętu?
+						</span>
+						<br />
+						Nawet najlepiej przygotowani mogą napotkać problemy techniczne.
+						Zapytaj, czy fotograf ma plan działania w przypadku awarii sprzętu.
+						Dobry profesjonalista zawsze będzie miał zapasowy plan, aby zapewnić
+						ci spokojną i udaną sesję fotograficzną.
+					</p>
+					<p>
+						<span className="keyword">
+							Czy możemy wspólnie zaplanować harmonogram sesji zdjęciowych?
+						</span>
+						<br />
+						Warto omówić z fotografem szczegóły dotyczące harmonogramu
+						fotografii podczas ślubu. Może to obejmować określenie czasu na
+						przygotowania, ceremonię, sesję zdjęciową oraz przyjęcie. Wspólne
+						planowanie pozwoli uniknąć niepotrzebnego pośpiechu i zapewni, że
+						wszystkie ważne momenty zostaną uwiecznione.
+					</p>
+					<p>
+						<span className="keyword">
+							Czy oferujesz usługi retuszu i edycji zdjęć?
+						</span>
+						<br />
+						Dobry fotograf powinien mieć umiejętności edycji i retuszu zdjęć,
+						aby sprawić, że będą one wyglądać jeszcze lepiej. Zapytaj o proces
+						edycji, czy obejmuje on retuszowanie skóry, korektę kolorów i
+						usuwanie niechcianych elementów z obrazu. Upewnij się, że efekt
+						końcowy będzie zgodny z Twoimi oczekiwaniami.
+					</p>
+				</section>
+
+				<section>
+					<h2>Referencje i Recenzje</h2>
+					<p>
+						<span className="keyword">
+							Czy mogę zobaczyć referencje od poprzednich klientów?{" "}
+						</span>
+						<br />
+						Recenzje od innych par ślubnych mogą być cennym źródłem informacji
+						na temat pracy danego fotografa. Poproś o referencje lub sprawdź
+						opinie online, aby dowiedzieć się, jakie są doświadczenia innych
+						klientów. Pozytywne opinie będą potwierdzeniem, że wybrałeś
+						profesjonalistę, któremu możesz zaufać. Wybór odpowiedniego
+						<a
+							className="link_home"
+							href="https://bit.ly/3vmEJZZ "
+							target="_blank"
+						>
+							fotografa ślubnego
+						</a>
+						może być trudnym, ale ważnym zadaniem. Pamiętaj, że fotograf będzie
+						towarzyszył Ci w jednym z najważniejszych dni Twojego życia, dlatego
+						warto poświęcić czas na znalezienie idealnego kandydata. Zadając te
+						10 pytań, będziesz miał pewność, że wybierasz fotografa, który
+						zrozumie Twoje potrzeby i uwieczni każdy ważny moment tego
+						wyjątkowego dnia.
+					</p>
+				</section>
+			</main>
+			<footer>
+				<div className="social-media">
+					<a
+						href="https://www.facebook.com/people/KonRad-Fotografia/100066942833739/"
+						target="_blank"
+					>
+						Facebook
+					</a>
+					<a
+						href="https://www.instagram.com/konradk_fotografia/"
+						target="_blank"
+					>
+						Instagram
+					</a>
+					<a href="https://www.tiktok.com/@konradk_fotografia" target="_blank">
+						TikTok
+					</a>
+				</div>
+				<p>&copy; 2024 KonRad Fotografia</p>
+			</footer>
+		</>
+	);
+}
